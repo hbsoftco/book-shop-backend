c07a94adde2818c870d5b54639dfd3547b3a2c15
Hossein <hossein.bajan@gmail.com>
2024-05-11 21:23:07 +0330

    Add .nvmrc file

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ class UserController {
   static async getAllUsers(req: Request, res: Response): Promise<void> {
     try {
       // const books = await User.find();
-      const books = await [];
+      const books = await [{ name: 'hossein' }];
 
       res.status(200).json({
         message: 'All Users',
